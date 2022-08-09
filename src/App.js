3ad2979d1587c2a8422eff3a3443216473fe7cc5
Isaac Tullos <isaac.tullos@gmail.com>
2022-08-09 13:32:09 -0400

    Reusable React Components

--- src/App.js
+++ src/App.js
@@ -49,7 +49,12 @@ const App = () => {
     <div>
       <h1>My Hacker Stories</h1>
       
-      <Search search={searchTerm} onSearch={handleSearch} />
+      <InputWithLabel 
+        id="search"
+        label="Search"
+        value={searchTerm}
+        onInputChange={handleSearch}
+      />
 
       <hr />
 
@@ -85,15 +90,16 @@ const Item = ({ item }) => {
   )
 }
 
-const Search = ({ search, onSearch }) => {
+const InputWithLabel = ({ id, label, value, type = 'text', onInputChange }) => {
 
   return (
     <>
-      <label htmlFor="search">Search: </label>
-      <input id="search" 
-             type="text" 
-             value={search}
-             onChange={onSearch}
+      <label htmlFor={id}>{label}: </label>
+      
+      <input id={id} 
+             type={type} 
+             value={value}
+             onChange={onInputChange}
       />
     </>
   )
