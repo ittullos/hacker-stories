c3fd07eddd009afcaddc684c127fc3b5af6ce2a6
Isaac Tullos <isaac.tullos@gmail.com>
2022-08-09 13:54:04 -0400

    Imperative React

--- src/App.js
+++ src/App.js
@@ -52,6 +52,7 @@ const App = () => {
       <InputWithLabel 
         id="search"
         value={searchTerm}
+        isFocused
         onInputChange={handleSearch}
       >
         <strong>Search: </strong>
@@ -97,15 +98,27 @@ const InputWithLabel = ({
   value, 
   type = 'text', 
   onInputChange,
+  isFocused,
   children   
 }) => {
+
+  const inputRef = React.useRef();
+
+  React.useEffect(() => {
+    if (isFocused && inputRef.current) {
+      inputRef.current.focus()
+    }
+  }, [isFocused])
 
   return (
     <>
       <label htmlFor={id}>{children}</label>
-      <input id={id} 
+      {/* B */}
+      <input ref={inputRef}
+             id={id} 
              type={type} 
              value={value}
+             autoFocus={isFocused}
              onChange={onInputChange}
       />
     </>
